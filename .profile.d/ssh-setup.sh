1e6323295b5a98fc042d5dc058741d93ab8762f9
Wildhan Triherlian <triherlian@gmail.com>
2022-06-05 23:19:19 +0700

    commit

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -4,16 +4,16 @@
 echo $0: creating public and private key files
 
 # Create the .ssh directory
-mkdir -p ${HOME}/ssh
-chmod 700 ${HOME}/ssh
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
 
 # Create the public and private key files from the environment variables.
-echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/ssh/heroku_id_rsa.pub
-chmod 644 ${HOME}/ssh/heroku_id_rsa.pub
+echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/.ssh/heroku_id_rsa.pub
+chmod 644 ${HOME}/.ssh/heroku_id_rsa.pub
 
 # Note use of double quotes, required to preserve newlines
-echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/ssh/heroku_id_rsa
-chmod 600 ${HOME}/ssh/heroku_id_rsa
+echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_id_rsa
+chmod 600 ${HOME}/.ssh/heroku_id_rsa
 
 # Preload the known_hosts file  (see "version 2" below)
 # echo '|1|longstringofstuff= ecdsa-sha2-nistp256 more stuff=
