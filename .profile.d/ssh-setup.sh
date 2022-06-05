5ce0fe990f880eec51ce5a996df8da701e8d89b3
Wildhan Triherlian <triherlian@gmail.com>
2022-06-05 23:21:45 +0700

    commit

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -8,19 +8,19 @@ mkdir -p ${HOME}/.ssh
 chmod 700 ${HOME}/.ssh
 
 # Create the public and private key files from the environment variables.
-echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/.ssh/heroku_id_rsa.pub
-chmod 644 ${HOME}/.ssh/heroku_id_rsa.pub
+echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/.ssh/id_rsa.pub
+chmod 644 ${HOME}/.ssh/id_rsa.pub
 
 # Note use of double quotes, required to preserve newlines
-echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_id_rsa
-chmod 600 ${HOME}/.ssh/heroku_id_rsa
+echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/id_rsa
+chmod 600 ${HOME}/.ssh/id_rsa
 
 # Preload the known_hosts file  (see "version 2" below)
 # echo '|1|longstringofstuff= ecdsa-sha2-nistp256 more stuff=
 # |1|morestuff= ecdsa-sha2-nistp256 yetmorestuff=' > ${HOME}/ssh/known_hosts
 
 # Start the SSH tunnel if not already running
-SSH_CMD="ssh -v -f -i ${HOME}/ssh/heroku_id_rsa -N -L 3307:${REMOTE_MYSQL_HOST}:3306 ${REMOTE_USER}@${REMOTE_SITE}"
+SSH_CMD="ssh -v -f -i ${HOME}/.ssh/id_rsa -N -L 3307:${REMOTE_MYSQL_HOST}:3306 ${REMOTE_USER}@${REMOTE_SITE}"
 echo SSH_CMD
 
 PID=`pgrep -f "${SSH_CMD}"`
